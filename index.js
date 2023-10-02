b4dcc9fab6ed7d8539b69b4206f39ba9e35775f3
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-10-02 09:49:58 -0400

    lab complete

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const main = document.getElementById('main');
 main.remove();
 const newHeader = document.createElement("h1");
 newHeader.id = 'victory';
-newHeader.textContent = "Khrystyna is the champion";
+newHeader.textContent = "Khrystyna is the champion"
 
